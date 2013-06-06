94a278d1dfdd3e8418a156f1cb411cb454b108af
John W. Fail <fail@cenotaph.org>
2013-06-07 01:58:13 +0300

    experimenting with large carousel, can it be responsive?

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,7 +7,7 @@ class Project < ActiveRecord::Base
   has_attached_file :avatar, :styles => {:new_carousel => "960x400#", :medium => "400x400>", :thumb => "100x100>" },
           :path =>  ":rails_root/public/images/projects/:id/:style/:basename.:extension", 
         :url => "/images/projects/:id/:style/:basename.:extension"
-  has_attached_file :carousel, :styles => {:new_carousel => "960x400#", :full => "600x400#", :small => "300x200#"}, :path =>  ":rails_root/public/images/carousel/projects/:id/:style/:basename.:extension",
+  has_attached_file :carousel, :styles => {:largest => "1200x492#", :new_carousel => "960x400#", :full => "600x400#", :small => "300x200#"}, :path =>  ":rails_root/public/images/carousel/projects/:id/:style/:basename.:extension",
                           :url =>  "/images/carousel/projects/:id/:style/:basename.:extension"
   has_many :resources
   translates :description

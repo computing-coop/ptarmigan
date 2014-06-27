9cd43739f2a0c6d9c5db59888b43f55785605e4c
John W. Fail <fail@cenotaph.org>
2014-06-27 13:24:36 +0300

    og tags in podcast page

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -9,6 +9,18 @@ class PodcastsController < ApplicationController
   
   def show
     @podcast = Podcast.find(params[:id])
+    set_meta_tags :og => {
+        :title => "Podcast: " + @podcast.name,
+        :type  => "ptarmigan:podcast",
+        :image => @podcast.event.nil? ? false : "http://ptarmigan.ee" + @podcast.event.avatar.url(:small),
+        :url   =>  url_for(@podcast) },
+
+        :canonical => url_for(@podcast),
+        :keywords => 'Tallinn,Estonia,Ptarmigan,podcasts,discussion,art,dialogue,social practice',
+        :description => @podcast.description,
+        :title => "Podcast: " + @podcast.name
+
+
   end
   
 

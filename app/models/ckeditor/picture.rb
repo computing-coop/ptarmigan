4898801d9d9dc208226b1b20e119e0db1935ee9c
John W. Fail <john@pixelache.ac>
2015-12-29 16:28:27 +0200

    paperclip ckeditor validation addded

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -5,9 +5,10 @@ class Ckeditor::Picture < Ckeditor::Asset
                     :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
 	                  :styles => { :content => '800>', :thumb => '118x100#' }
 	
-	validates_attachment_size :data, :less_than => 2.megabytes
+	validates_attachment_size :data, :less_than => 5.megabytes
 	validates_attachment_presence :data
-	
+  validates_attachment_content_type :data, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  alias_attribute :name, :title
 	def url_content
 	  url(:content)
 	end

1f597d768ae9a9fb67728e21677ad4aacc7bdfc4
John W. Fail <john@pixelache.ac>
2015-12-29 11:17:01 +0200

    ckeditor must play nice in production

--- config/application.rb
+++ config/application.rb
@@ -62,7 +62,6 @@ module Ptarmigan
     config.active_support.escape_html_entities_in_json = true
     config.assets.precompile += %w( common.css print.css  jwplayer.js jwplayer.html5.js cms.css cms.js madhouse/stylesheets/staff.css madhouse/javascripts/staff.js )
 
-    config.assets.precompile += Ckeditor.assets
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types

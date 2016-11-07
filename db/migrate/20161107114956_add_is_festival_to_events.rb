da0fd8bac6b3fe1909a9583f5094ed29c0f6d6ca
John W. Fail <john@pixelache.ac>
2016-11-07 14:21:45 +0200

    try to fix in migration"

--- db/migrate/20161107114956_add_is_festival_to_events.rb
+++ db/migrate/20161107114956_add_is_festival_to_events.rb
@@ -1,5 +1,6 @@
 class AddIsFestivalToEvents < ActiveRecord::Migration[5.0]
   def change
+    execute("update events set enddate = null where enddate = '0000-00-00 00:00:00'")
     add_column :events, :is_festival, :boolean
   end
 end

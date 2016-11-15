93851e906f9933bd9162aa85f1b6ef8548fc443f
John W. Fail <john@pixelache.ac>
2016-11-15 03:24:31 +0200

    asset manifest

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,13 @@
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+//= link_tree ../fonts
+//= link_tree ../themes/creativeterritories
+//= link_tree ../themes/donekino
+//= link_tree ../themes/ee
+//= link_tree ../themes/fi
+//= link_tree ../themes/kimmomodig
+//= link_tree ../themes/kompass
+//= link_tree ../themes/kuulutused
+//= link_tree ../themes/madhouse
+ 

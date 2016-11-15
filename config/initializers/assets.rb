31fab6ec0a33eada9bbe1b5122a536f6d21e5a96
John W. Fail <john@pixelache.ac>
2016-11-15 03:40:02 +0200

    one more time trying to fix font digests

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "themes", "creativeterritories", "fonts")
 Rails.application.config.assets.precompile += %w( ckeditor/* creativeterritories/stylesheets/staff.css creativeterritories/javascripts/staff.css creativeterritories/fonts/*)
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$

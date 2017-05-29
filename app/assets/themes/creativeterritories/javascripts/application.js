3a63ccaedac472598834ce5f3f7d4d1a2d949da0
John W. Fail <john@pixelache.ac>
2017-05-29 12:28:16 +0300

    column highlighting fix

--- app/assets/themes/creativeterritories/javascripts/application.js
+++ app/assets/themes/creativeterritories/javascripts/application.js
@@ -6,7 +6,9 @@
 // = require moment/lv.js
 //= require fullcalendar
 //= require fullcalendar/lang/lv
-// = require featherlight
+//= require featherlight
+
+
   var dbids = [];
   var markers = [];
     var infowindows = [];
@@ -205,3 +207,5 @@ function load_content(map,marker,infowindow,railsid){
     }
   });
 }   
+
+;!function(e){e.fn.classes=function(t){var n=[];e.each(this,function(e,t){var r=t.className.split(/\s+/);for(var i in r){var s=r[i];if(-1===n.indexOf(s)){n.push(s)}}});if("function"===typeof t){for(var r in n){t(n[r])}}return n}}(jQuery);
